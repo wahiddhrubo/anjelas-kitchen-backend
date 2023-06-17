7d9fa098df9d976634e4876d0b2f3f8a2f17eb6e
wahidddhrubo <wahiddhrubo@gmail.com>
2023-06-17 20:26:36 +0600

    Vercel

--- Backend/server.js
+++ Backend/server.js
@@ -3,11 +3,11 @@ const dotenv = require("dotenv");
 const connectToDb = require("./config/db.js");
 
 //CONFIG
-dotenv.config({ path: "Backend/config/config.env" });
+dotenv.config({ path: "config/config.env" });
 
 //DATABASE CONNECTION
 connectToDb();
 
 app.listen(process.env.PORT, () => {
-	console.log("Server is running!!!");
+  console.log("Server is running!!!");
 });
