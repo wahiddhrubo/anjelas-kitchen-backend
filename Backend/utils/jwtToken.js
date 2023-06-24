45bfd899c91cc9d50af81b3e0e161b5b8f7f2ecf
wahidddhrubo <wahiddhrubo@gmail.com>
2023-06-24 16:42:02 +0600

    First Commit

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -6,6 +6,10 @@ const sendToken = (user, res, statusCode) => {
       new Date().getTime() + process.env.COOKIE_EXPIRE * 24 * 60 * 60
     ),
     httpOnly: true,
+    domain:
+      process.env.NODE_ENV === "development"
+        ? ".localhost"
+        : process.env.FRONTEND_DOMAIN,
   };
 
   res.status(statusCode).cookie("token", token, options).json({
