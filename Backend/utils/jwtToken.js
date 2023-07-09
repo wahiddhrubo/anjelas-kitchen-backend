23be3a8cf4359e87bd42e8194675ea7f48998769
wahidddhrubo <wahiddhrubo@gmail.com>
2023-07-09 15:40:32 +0600

    Cookie

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -1,3 +1,5 @@
+const { none } = require("./cloudinary");
+
 const sendToken = (user, res, statusCode) => {
   const token = user.getJwtToken();
 
@@ -6,11 +8,10 @@ const sendToken = (user, res, statusCode) => {
       new Date().getTime() + process.env.COOKIE_EXPIRE * 24 * 60 * 60
     ),
     httpOnly: true,
-    domain:
-      process.env.NODE_ENV === "development"
-        ? ".localhost"
-        : process.env.FRONTEND_DOMAIN,
+    domain: ".localhost",
+    sameSite: "none",
   };
+  console.log({ statusCode, token, options });
 
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
